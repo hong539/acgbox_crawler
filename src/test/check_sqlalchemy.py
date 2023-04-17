cea1d42c6cb116e3696c97f4b91948062bfbc62f
hong <j50508@gmail.com>
2023-04-17 21:33:19 +0800

    Up codes

--- src/test/check_sqlalchemy.py
+++ src/test/check_sqlalchemy.py
@@ -1,7 +1,14 @@
 import yaml
 from sqlalchemy import create_engine
+from sqlalchemy import Table, MetaData
 
 with open('config.yaml', 'r') as f:
     config = yaml.safe_load(f)
 
 engine = create_engine(f"mysql+pymysql://{config['user']}:{config['password']}@{config['host']}:{config['port']}/{config['database']}?charset=utf8mb4")
+metadata = MetaData()
+users_table = Table('users', metadata, autoload=True, autoload_with=engine)
+
+result = engine.execute(users_table.select())
+for row in result:
+    print(row)
