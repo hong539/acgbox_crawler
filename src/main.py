f6c99430f90f925d623e602c5323c1d53d3005c3
hong <j50508@gmail.com>
2023-04-22 07:30:46 +0800

    Up codes

--- src/main.py
+++ src/main.py
@@ -35,37 +35,33 @@ def modfy_data():
         # print(ss)
         gg = ss['ACG_name'].str.replace("動畫", "")
         # print(gg)
-
         df_acg = pd.DataFrame(columns=['Anime_name'])
         df_acg['Anime_name'] = gg
         print(df_acg)
         df_acg.to_sql('anime_favorites', engine, if_exists='append', index=False)
 
-def parser(seed_url, number, username):
+def parser(seed_url, username, number):
         sleep(3)
         target_url = seed_url + str(number) + "&owner=" + str(username) + "&tab=&m="
         print(target_url)
         
         r = requests.get(target_url, headers=HEADERS)
         soup = BeautifulSoup(r.text, 'html.parser')
-        p = soup.find_all(class_="acgboxname")
-        
+        p = soup.find_all(class_="acgboxname")        
         catch_text = []
-
         for item in p:
                 s = item.select_one('a').text
                 catch_text.append(s)
-                
-        
+
         # print(catch_text)
         df_acg = pd.DataFrame(columns=['ACG_name'])
-
         df_acg['ACG_name'] = catch_text
-
         print(df_acg)
 
+        #src:https://docs.sqlalchemy.org/en/20/core/engines.html#sqlalchemy.create_engine
         # engine_url = "mysql+pymysql://user:passwd@host:3306/gamer_crawler"
         # engine = create_engine(engine_url, echo=True)
+        #src:https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_sql.html?highlight=to_sql#pandas.DataFrame.to_sql
         # df_acg.to_sql('acg_collections', engine, if_exists='append', index=False)
 
 if __name__ == "__main__":        
@@ -73,5 +69,5 @@ if __name__ == "__main__":
         #You should be careful when using range() in for loop!
         #Where to stat and where to stop?
         for c in range(1, c+1):
-                parser(a, c, b)
+                parser(a, b, c)
         # modfy_data()
